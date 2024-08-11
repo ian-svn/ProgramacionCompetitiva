309b076d4246a536d717cad431294f7078cad598
Ian Vila <iansantiago.ian@gmail.com>
2024-08-11 01:29:41 -0300

    EjsResueltos v3

---
+++ variosLvls/desalambrando.cpp
@@ -0,0 +1,95 @@
+// desalambrando_ian-svn
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int desalambrado(int N, vector<int>& a, vector<int>& b, vector<int>& c, int& m) {
+    vector<int> eli;
+
+    for (int x = 1; x < N; x++) {
+        vector<int> postesAdyas;
+        
+        for (int y = 0; y < m; y++) {
+            if (a[y] == x || b[y] == x) {
+                int posteAd = (a[y] == x) ? b[y] : a[y];
+                postesAdyas.push_back(posteAd);
+            }
+        }
+
+        if (postesAdyas.size() > 2) {
+            for(int poste : postesAdyas){
+                int cant=0;
+                for (int y = 0; y < m; y++) {
+                    if (a[y] == poste || b[y] == poste) {
+                        cant++;
+                    }
+                }
+
+                if(cant>2){
+                    for (int y = 0; y < m; y++) {
+                        if ((a[y] == poste && b[y] == x)||(a[y] == x && b[y] == poste)) {
+                            eli.push_back(c[y]);
+                            a.erase(a.begin()+y);
+                            b.erase(b.begin()+y);
+                            c.erase(c.begin()+y);
+                            m=m-1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    int aux=99999999,pos;
+
+    for (int x = 0; x < m; x++) {
+        if(aux > c[x]){
+            aux = c[x];
+            pos = x;
+        }
+    }
+    eli.push_back(c[pos]);
+    a.erase(a.begin()+pos);
+    b.erase(b.begin()+pos);
+    c.erase(c.begin()+pos);
+    m=m-1;
+
+    int acu=0;
+    
+    acu+=aux;
+
+    for (int x = 0; x < eli.size(); x++) {
+        acu+=eli[x];
+    }
+
+    for(int x=0;x<m;x++){
+        cout << a[x] << " "<<b[x]<< " " << c[x]<<endl;
+    }
+        
+    return acu;
+}
+
+int main() {
+    int N, m;
+    vector<int> A, B, C;
+
+    cin >> N >> m;
+
+    for (int x = 0; x < m; x++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        A.push_back(a);
+        B.push_back(b);
+        C.push_back(c);
+        if (a == b) {
+            return 0;
+        }
+        if (a > N || b > N) {
+            return 0;
+        }
+    }
+
+    cout << desalambrado(N, A, B, C, m);
+
+    return 0;
+}
--- variosLvls/envido.cpp
+++ variosLvls/envido.cpp
@@ -1,14 +1,30 @@
-// envido_ian-svn  
+// envido_ian-svn. lvl 1, 2 y 3
 
 #include <bits/stdc++.h>
 using namespace std;
 
+int cartaMasAlta(int numero1, int numero2, int numero3){
+    int cA = max(max(numero1,numero2),max(numero2,numero3));
+    if(numero1>=10||numero2>=10||numero3>=10){
+        if(numero1>=10&&numero2>=10&&numero3>=10){
+            return 0;
+        }
+        if(numero1>=10||numero2>=10){
+            cA=(numero1>=10) ? numero2 : numero1;
+        } else if(numero3>=10||numero2>=10){
+            cA=(numero3>=10) ? numero2 : numero3;
+        }
+    }
+    return cA;
+}
+
 int envido(int numero1, string palo1,int  numero2, string  palo2,int  numero3, string  palo3){
+    
     if((palo1==palo2)||(palo2==palo3)||(palo3==palo1)){
         string paloIgual; int num1; int num2;
         if(palo1==palo2&&palo2==palo3){
-            num1=max(numero1,numero2);
-            num2=max(numero2,numero3);
+            num1=(numero1<10&&numero2<10) ? max(numero1,numero2) : min(numero1,numero2);
+            num2=(numero2<10&&numero3<10) ? max(numero2,numero3) : min(numero2,numero3);
             if(num2==num1){ num2=numero3;}
         } else if((palo1==palo2)){
             num1=numero1;
@@ -20,19 +36,21 @@ int envido(int numero1, string palo1,int  numero2, string  palo2,int  numero3, s
             num1=numero1;
             num2=numero3;
         } else {
-            return max(max(numero1,numero2),max(numero2,numero3));
-        } 
+            return cartaMasAlta(numero1, numero2, numero3);
+        }
 
         if((num1<10&&(num2<10))&&num1<=12&&num2<=12&&num1>=1&&num2>=1){
             return num1+num2+20;
-        } if((num1>=10||(num2>=10))&&num1<=12&&num2<=12&&num1>=1&&num2>=1){
+        } else if(num1>=10&&num2>=10){
+            return 0;
+        } else if((num1>=10||(num2>=10))&&num1<=12&&num2<=12&&num1>=1&&num2>=1){
             int numMenor = (num1<10) ? numMenor=num1 : numMenor=num2;
             return 20 + numMenor;
-        }
+        } 
 
     }
 
-    return max(max(numero1,numero2),max(numero2,numero3));
+    return cartaMasAlta(numero1, numero2, numero3);
 }
 
 int main(){
@@ -43,7 +61,28 @@ int main(){
     cin >> numero2 >> palo2;
     cin >> numero3 >> palo3;
 
-    cout << envido(numero1, palo1, numero2, palo2, numero3, palo3);
+    if(!(numero1>=1&&numero1<=12&&numero1!=8&&numero1!=9)){
+        return 2;
+    }
+    if(!(numero2>=1&&numero2<=12&&numero2!=8&&numero2!=9)){
+        return 4;
+    }
+    if(!(numero3>=1&&numero3<=12&&numero3!=8&&numero3!=9)){
+        return 5;
+    }
+    
+    if(palo1!="oros"&&palo1!="copas"&&palo1!="espadas"&&palo1!="bastos"){
+        return 6;
+    }
+    if(palo2!="oros"&&palo2!="copas"&&palo2!="espadas"&&palo2!="bastos"){
+        return 7;
+    }
+    if(palo3!="oros"&&palo3!="copas"&&palo3!="espadas"&&palo3!="bastos"){
+        return 8;
+    }
+    int res = envido(numero1, palo1, numero2, palo2, numero3, palo3);
+
+    cout << res << endl;
 
     return 0;
 }
