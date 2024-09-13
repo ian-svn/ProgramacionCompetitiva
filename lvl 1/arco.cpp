6631f81de47b67c11ff6570e71b40c4ad93b53df
Ian Vila <iansantiago.ian@gmail.com>
2024-09-13 17:35:54 -0300

    EjsResueltos v7.2

--- lvl 1/arco.cpp
+++ lvl 1/arco.cpp
@@ -1,27 +1,27 @@
-    // arco_ian-svn 17:13 29
+// arco_ian-svn
 
-    #include <bits/stdc++.h>
-    using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-    string arco(int x,int y){
-        string rest;
-        
-        if(x>0&&x<732&&y<232){
-            rest="GOL";
-        } else if(x>=0&&x<=732&&y<=232){
-            rest="PALO";
-        } else {
-            rest="AFUERA";
-        }
-
-        return rest;
+string arco(int x,int y){
+    string rest;
+    
+    if(x>0&&x<732&&y<232){
+        rest="GOL";
+    } else if(x>=0&&x<=732&&y<=232){
+        rest="PALO";
+    } else {
+        rest="AFUERA";
     }
 
-    int main(){
-        int x, y;
-        cin >> x >> y;
-        if(x>1000||x<-1000||y>1000||y<-1000){
-            return 0;
-        }
-        cout << arco(x,y);
+    return rest;
+}
+
+int main(){
+    int x, y;
+    cin >> x >> y;
+    if(x>1000||x<-1000||y>1000||y<-1000){
+        return 0;
     }
+    cout << arco(x,y);
+}
